948e27067db4afd80638f03420b3bc983035813e
Abdulwadod <ai.wadod@hotmail.com>
2026-07-01 20:40:57 +0300

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// مسارات نسبية عشان يشتغل من أي مكان (GitHub Pages / Netlify / أي مجلد)
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "./",
-});
